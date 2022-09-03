3c54bd99e37363c06ce76bbfd7a05cd4d8b5de53
Marcos Leiva <89143317+MarquitosLev@users.noreply.github.com>
2022-09-03 12:14:11 -0300

    Delete Categoria.py

--- Categoria.py
+++
@@ -1,9 +0,0 @@
-class Categoria:
-    # nombre = ""
-
-    def __init__(self, nombre):
-        self.nombre = nombre
-
-    def __str__(self) -> str:
-        return "- Nombre de Categoria: %s"% self.nombre 
-
