06730a8466873426001d5a830a0546a8bdb92ccb
Leandro González Fister <leangonzalezfs04@gmail.com>
2022-09-06 11:30:15 -0300

    Modificaciones
    Solo  hice modificaciones para que la presentación se vea bien. Lo único que no entiendo, es por qué, al menos en mi  caso, después de que se imprime la información de un libro, sale la palabra "None" en la línea siguiente. Todo lo demás está bien.

--- Ejercicio_1/libros_cliente.py
+++ Ejercicio_1/libros_cliente.py
@@ -6,34 +6,37 @@ class libro_Cliente:
 
     def imprimir_libros(self, libros: list) -> None:
         for i in libros: 
-            print("*" * 25)
+            print("\n" + "*" * 25 + "\n")
             print(i)
             
     def filtrar_por_categoria(self, libros: list, cat: Categoria) -> list:
         for i in libros:
             if i.categoria == cat: 
-                print("*" * 25)
+                print("\n" + "*" * 25 + "\n")
                 print(i)
     
     def filtrar_por_autor(self, libros : list, autor : Autor)-> list:
         for i in libros:
             if i.autor == autor: 
-                print("*" * 25)
+                print("\n" + "*" * 25 + "\n")
                 print(i)
     
     def filtrar_por_anio(self, libros :  list, anio :  int)-> list:
         for i in libros:
             if i.anio_publi == anio: 
-                print("*" * 25)
+                print("\n" + "*" * 25 + "\n")
                 print(i)
 
 l1 = Libro(4, "HP", "JKR", "Fantasia", 1991)
-l2 = Libro(11, "Joker", "El bromas", "Humor", 1996)
+l2 = Libro(11, "Joker", "Jerry Robinson", "comic", 1940)
 l3 = Libro(13, "Señor de los anillos", "JR Tolkien", "Fantasia", 1996)
 f = libro_Cliente()
 lista = [l1, l2, l3]
-# print(f.imprimir_libros(lista))
-# print(f.filtrar_por_anio(lista, 1991))
-# print(f.filtrar_por_autor(lista, "JKR"))
-# print(f.filtrar_por_categoria(lista, "Fantasia"))
-# Quitar # para probar
+
+print(f.imprimir_libros(lista))
+print("\n filtrado por año: ")
+print(f.filtrar_por_anio(lista, 1996))
+print("\n filtrado por autor: ")
+print(f.filtrar_por_autor(lista, "JR Tolkien"))
+print("\n filtrado por categoría: ")
+print(f.filtrar_por_categoria(lista, "comic"))
