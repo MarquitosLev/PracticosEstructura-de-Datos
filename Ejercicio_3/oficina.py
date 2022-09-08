f81ce731c8b707f34233a38d93f0c79e3697465c
Leandro González Fister <leangonzalezfs04@gmail.com>
2022-09-08 12:17:41 -0300

    Ejercicio 3 del trabajo

---
+++ Ejercicio_3/oficina.py
@@ -0,0 +1,21 @@
+from datetime import date, datetime
+
+class Oficina:
+
+    def __init__(self, Nombre : str, HoraEntrada : datetime.time, HoraSalida : datetime.time) -> None:
+        self.Nombre = Nombre
+        self.HoraEntrada = HoraEntrada
+        self.HoraSalida = HoraSalida
+
+    def __str__(self) -> str:
+        return "-Nombre: %s\n -HoraEntrada: %s\n -HoraSalida: %s" % (self.Nombre, self.HoraEntrada, self.HoraSalida)
+    
+    def __repr__(self) -> str:
+        return "-Nombre: %s\n -HoraEntrada: %s\n -HoraSalida: %s" % (self.Nombre, self.HoraEntrada, self.HoraSalida)
+    
+    def __eq__(self, otro: object) -> bool:
+        if self.Nombre == otro.Nombre and self.HoraEntrada == otro.HoraEntrada and self.HoraSalida == otro.HoraSalida:
+            return True
+        else:
+            return False
+    
