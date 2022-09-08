3c4c0cfbd237bbf3c1e2954a151ce380915e7fcf
Marcos <marcleiva623@gmail.com>
2022-09-08 19:31:49 -0300

    cosas

--- Ejercicio_2/Mascota.py
+++ Ejercicio_2/Mascota.py
@@ -1,17 +1,16 @@
-from typing_extensions import Self
 from Persona import Persona
 from Raza import Raza
 class Mascota:
     
-    def __init__(self, num_registro:int, nombre:str, Raza:Raza, anio_nacimiento:int, Persona:Persona):
+    def __init__(self, num_registro:int, nombre:str, Raza:Raza, anio_nacimiento:int, duenio:Persona):
         self.num_registro = num_registro
         self.nombre = nombre
         self.raza = Raza
         self.anio_nacimiento = anio_nacimiento
-        self.persona = Persona
+        self.duenio = Persona
 
     def __str__(self):
-        return "- N° Registro: %s\n- Nombre: %s\n- Raza: %s\n- Anio de nacimiento: %s\n- Duenio/amo %s" % (self.num_registro, self.nombre, self.raza, self.anio_nacimiento, self.persona)
+        return "- N° Registro: %s\n- Nombre: %s\n- Raza: %s\n- Anio de nacimiento: %s\n- Duenio/amo %s" % (self.num_registro, self.nombre, self.raza, self.anio_nacimiento, self.duenio)
 
     def __eq__(self, otro):
         if self.num_registro == otro.num_registro and self.nombre == otro.nombre and self.anio_nacimiento == otro.anio_nacimiento:
@@ -19,5 +18,5 @@ class Mascota:
         else: return False    
 
     @property
-    def edad() -> int:
-        return 2022 - Self.anio_nacimiento
+    def edad(self):
+        return 2022 - self.anio_nacimiento
--- Ejercicio_2/bichitos_cliente.py
+++ Ejercicio_2/bichitos_cliente.py
@@ -1,8 +1,16 @@
 #bichitos_cliente
+from Mascota import Mascota
+from Persona import Persona
+from Raza import Raza
+
 def imprimir(mascotas : list) -> None:
     for i in mascotas: print (i)
 
 def filtrar_gerontes(mascotas :  list)   -> list :
     for i in mascotas:
-        if i.edad() >= 13:
+        if i.edad >= 13:
             print(i)
+
+n = Mascota(5, "Tobby", "Salchi", 2000, "Mica")
+print(n.edad)
+print(filtrar_gerontes([n]))
