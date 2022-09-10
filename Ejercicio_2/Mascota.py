6ce521f32622ad4b5e2a6542c80f7dd89532369d
gosti8 <112736214+gosti8@users.noreply.github.com>
2022-09-10 15:02:26 -0300

    punto 2 casi casi
    acá era que había que subirlo xd

--- Ejercicio_2/Mascota.py
+++ Ejercicio_2/Mascota.py
@@ -1,22 +1,25 @@
 from Persona import Persona
 from Raza import Raza
 class Mascota:
-    
-    def __init__(self, num_registro:int, nombre:str, Raza:Raza, anio_nacimiento:int, duenio:Persona):
+    Anio_actual = 2022 
+    def __init__(self, num_registro:int, nombre:str, Raza:Raza, anio_nacimiento:int, Persona:Persona):
         self.num_registro = num_registro
         self.nombre = nombre
         self.raza = Raza
         self.anio_nacimiento = anio_nacimiento
-        self.duenio = Persona
+        self.persona = Persona
 
     def __str__(self):
-        return "- N° Registro: %s\n- Nombre: %s\n- Raza: %s\n- Anio de nacimiento: %s\n- Duenio/amo %s" % (self.num_registro, self.nombre, self.raza, self.anio_nacimiento, self.duenio)
+        return "- N° Registro: %s\n- Nombre: %s\n- Raza: %s\n- Anio de nacimiento: %s\n- Duenio/amo %s" % (self.num_registro, self.nombre, self.raza, self.anio_nacimiento, self.persona)
 
     def __eq__(self, otro):
         if self.num_registro == otro.num_registro and self.nombre == otro.nombre and self.anio_nacimiento == otro.anio_nacimiento:
             return True
-        else: return False    
-
-    @property
-    def edad(self):
-        return 2022 - self.anio_nacimiento
+        else: return False  
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    
+    @staticmethod
+    def calc_edad(self):
+        return Mascota.Anio_actual - self.anio_nacimiento
