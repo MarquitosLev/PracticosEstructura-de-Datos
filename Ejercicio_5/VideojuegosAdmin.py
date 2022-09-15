3aff54482f9bbe5e44c901d1af8189188c65f05c
Marquitos <marcleiva623@gmail.com>
2022-09-15 13:32:20 -0300

    Reformes de metodos. faltan todavia :v

---
+++ Ejercicio_5/VideojuegosAdmin.py
@@ -0,0 +1,44 @@
+from videojuego import Videojuego
+from VideojuegoAdminAbstract import VideojuegosAdminAbstract
+from Empresa import Empresa
+from Genero import Genero
+
+class VideojuegosAdmin(VideojuegosAdminAbstract):
+
+    def agregar(self, videojuego : Videojuego) -> None:
+        """Agrega el parámetro al final de videojuegos
+        Args:
+        videojuego (Videojuego): instancia de clase videojuego que se
+        va a agregar al final de la lista de videojuegos.        """
+        self.videojuegos.append(videojuego)
+    
+    def __str__(self) -> str:
+        """Concatena en un str todos los videojuegos del catálogo."""
+        cadena = ""
+        for game in self.videojuegos:
+            cadena += f"{game.titulo}, "
+        return cadena
+        
+    def filtrar_por_desarrolladora(self, desarrolladora: Empresa) -> list:
+        """Devuelve los videojuegos desarrollados por la empresa pasada por parámetro."""        
+        pass
+
+    def filtrar_por_distribuidora(self, distribuidora: Empresa) -> list:
+        """Devuelve los videojuegos distribuídos por la empresa pasada por parámetro."""
+        pass   
+
+    def filtrar_por_genero(self, genero: Genero) -> list:
+        """Devuelve los videojuegos del género pasado por parámetro. """        
+        pass        
+     
+    def cantidad_por_plataforma(self) -> list:
+        """Indica la cantidad de videojuegos por plataforma. """        
+        pass        
+    
+    def ordenar_titulo(self) -> None:
+        """Ordena los videojuegos por titulo."""        
+        pass                
+    
+    def ordenar_mejores_primero(self) -> None:
+        """Ordena los videojuegos por ranking descendente. """     
+        pass
