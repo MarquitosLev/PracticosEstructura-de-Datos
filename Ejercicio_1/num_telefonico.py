adaa9e53296dbf2c7b3a53f7b06fd3aa7a92f819
gosti8 <sebastiandanielgosti8@gmail.com>
2022-09-28 21:55:30 -0300

    Ejercicio 1 Terminado Ahora si

--- Ejercicio_1/num_telefonico.py
+++ Ejercicio_1/num_telefonico.py
@@ -1,10 +1,22 @@
-#N1 = "(0345) 154123456" 
-#N2 = "+5493454123456" 
-#N3 = "3454123456" 
-#N4 = "+54011123456" 
-#N5 = "34564123456" 
+N1 = "(0345) 154123456" 
+N2 = "+5493454123456" 
+N3 = "3454123456" 
+N4 = "+54011123456" 
+N5 = "34564123456" 
 
-Num = input("Ingrese su número de teléfono: ")
 NValidos = ("(0345) 154", "+5493454", "3454")
-Aux = Num.startswith(NValidos, 0, len(Num))
+
+Aux = N1.startswith(NValidos, 0, len(N1))
+print (Aux)
+
+Aux = N2.startswith(NValidos, 0, len(N2))
+print (Aux)
+
+Aux = N3.startswith(NValidos, 0, len(N3))
+print (Aux)
+
+Aux = N4.startswith(NValidos, 0, len(N4))
+print (Aux)
+
+Aux = N5.startswith(NValidos, 0, len(N5))
 print (Aux)
