bcf8a6156f060724882afca2974f3d95d55b4987
Marcos Leiva <marcleiva623@gmail.com>
2022-10-01 22:24:57 -0300

    Cambios en metodos. Preguntar al profe algunas cosas de nodos

--- Ejercicio_6/DoubleLinkedList.py
+++ Ejercicio_6/DoubleLinkedList.py
@@ -1,4 +1,5 @@
 from csv import list_dialects
+from operator import getitem
 from typing import Any, Iterator, List
 from DoubleLindedListAbstract import DoubleLinkedListAbstract
 from ListNode import ListNode
@@ -36,13 +37,14 @@ class DoubleLinkedList(DoubleLinkedListAbstract):
         "Agrega un elemento al final de la lista segun parametro pasado"
 
         if self.is_empty():
-            self._curr = ListNode(elem) #Agrega nuevo nodo
+            self._curr = ListNode(elem, None, None)
+            self._size += 1
+            return
         else:
-            #NO FUNCA ESTO, NO DEBE TENER VARIABLE AUXILIAR
-            while self._curr:
-                self._curr = self._curr.next
-            
-            self._curr = ListNode(elem)
+            current = self._curr
+            while current.next is not None: #Va al ultimo elemento de la lista enlazada
+                current = current.next 
+            current.next = ListNode(elem)
         self._size += 1
 
     def __getitem__(self, key: int) -> Any:
@@ -56,31 +58,57 @@ class DoubleLinkedList(DoubleLinkedListAbstract):
         if self.is_empty():
             raise Exception("La lista esta vacia")
         
-        test = 0
+        cont = 0
         while self._curr:
-            if test == key:
+            if cont == key:
                 return self._curr.element
-            
-            self._curr.next
-            test += 1
-
+            self._curr = self._curr.next
+            cont += 1
 
     def __delitem__(self, key: int) -> None:
         """Elimina de la estructura el elemento ubicado en la posición key.
         Args: key (int): posición cuyo nodo se va a quitar de la estructura.
         Raises: Exception: Arroja excepción si el índice está fuera de rango."""
+        
+        if key < 0 or key >= self._size:
+            raise Exception("Key fuera de rango")
+        if self.is_empty():
+            raise Exception("La lista esta vacia")
+        
+        #Eliminacion 
         pass
 
     def __iter__(self) -> Iterator[Any]:
         """ Visita desde el principio hacia el final todos los nodos de la lista y
         retorna sus elementos.
         Yields: Iterator[Any]: Cada uno de los elementos de los nodos de lista."""
-        pass
+        self[0]
+        n = self._curr
+        while n is not None:
+            yield n.element
+            n = n.next
 
-#PRUEBA, APPEND NO ANDA Y EL STR TIENE VARIABLE
 lista = DoubleLinkedList()
+lista.append(1)
 lista.append("Hola")
 lista.append(666)
 lista.append("Elemento")
 lista.append(19)
 print(lista)
+
+#Prueba metodo __getitem__
+print("__getitem__ : ", lista[3]) 
+
+i = 0
+for elem in lista:
+    print(f"lista[{i}]: {elem}")
+    i += 1
+
+#Prueba metodo __getitem__ pero el nodo queda en otra posicion (Preguntar al profe)
+print("__getitem__ : ", lista[3]) 
+
+
+
+
+
+
