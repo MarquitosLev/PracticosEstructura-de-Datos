fe3f5f65b83b3e53cc0619261fe365ce70191c15
Marcos Leiva <89143317+MarquitosLev@users.noreply.github.com>
2022-10-19 09:57:21 -0300

    Delete Ejercicio_6 directory

--- Ejercicio_6/ListNode.py
+++
@@ -1,8 +0,0 @@
-from typing import Any, Union
-
-class ListNode:
-    
-    def __init__(self, element : Any) -> None:
-        self.element = element
-        self.next = None
-        self.prev = None
