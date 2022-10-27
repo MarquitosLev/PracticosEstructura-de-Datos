90d16d3165cfa2ff3aff9124392e71bde0149c33
Marquitos Leiva <marcleiva623@gmail.com>
2022-10-27 00:48:58 -0300

    Agregados de los archivos punto 1

---
+++ Ejercicio_1/__pycache__/LinkedBinaryTreeExt.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+from typing import Any, List, Union
+from data_structures.trees import BinaryTreeNode
+
+class LinkedBinaryTreeExt():
+
+    def __init__(self) -> None:
+        pass
