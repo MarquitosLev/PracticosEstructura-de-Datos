60842834a44130c162bc2d471fb1993d792f90d6
gosti8 <sebastiandanielgosti8@gmail.com>
2022-10-28 01:05:00 -0300

    Punto 5  top

---
+++ Ejercicio_6/data_structures/__init__.py
@@ -0,0 +1,3 @@
+from .linear import ArrayStack, LinkedStack, ArrayQueue, LinkedQueue, LinkedList, ListNode
+from .trees import BinaryTreeNode, LinkedBinaryTree
+from .priority_queues import ArrayHeap, PriorityQueueBase
---
+++ Ejercicio_6/pq_stack__client.py
@@ -0,0 +1,7 @@
+from Priority_queue_stack import PriorityQueueStack
+
+PriorityQueueStack.push(2, 2)
+PriorityQueueStack.push(4)
+PriorityQueueStack.push(1)
+PriorityQueueStack.push(7)
+PriorityQueueStack.push(10)
