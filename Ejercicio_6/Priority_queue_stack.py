b310ae68601cc67a934eba04cc5e7e6566aced11
gosti8 <sebastiandanielgosti8@gmail.com>
2022-11-05 16:54:49 -0300

    Ultimas correcciones punto 5

--- Ejercicio_6/Priority_queue_stack.py
+++ Ejercicio_6/Priority_queue_stack.py
@@ -1,42 +1,30 @@
 from data_structures.priority_queues.priority_queue_base import PriorityQueueBase
 from data_structures.priority_queues.array_heap import ArrayHeap
-from typing import Any, List, Union
-from data_structures.linear.list_node import ListNode
+from typing import Any
 class PriorityQueueStack(ArrayHeap, PriorityQueueBase):
 
     def __init__(self) -> None:
         self.prioridades = []
-        self._head = 0
+        self._head = None
 
     def is_empty(self) -> bool:
-        """ Indica si la estructura está vacía o no.
-        Returns:
-        bool: True si está vacía. False en caso contrario.
-        """
+
         return len(self.prioridades) == 0
 
-    def Push(self, value: Any) -> None:
-        """Agrega el elemento elem en el tope de la pila.
+    def push(self, key: any, value: Any) -> None:
 
-        Args:
-            elem (Any): Nuevo elemento que se va agregar a la pila.
-        """
-        self._head = value
-        self.prioridades.append(value)
+        self._head = (key, value)
+        self.prioridades.append((key, value))
 
         
-    def Top(self) -> int:
-        """Devuelve (sin quitar) el elemento ubicado en el tope de la pila.
-        Arroja una excepción si la pila está vacía.
-        """
+    def top(self) -> int:
+
         if(self.is_empty()):
             raise Exception("La pila se encuentra vacía")
         return self._head
         
-    def Pop(self) -> Any:
-        """Quita y devuelve el elemento ubicado en el tope de la pila.
-        Arroja una excepción si la pila está vacía
-        """
+    def pop(self) -> Any:
+
         if(self.is_empty()):
             raise Exception("La pila se encuentra vacía")
 
